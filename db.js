e0372bb332b56b539213f86dca870a839b3de261
cyclone77 <fillchar@live.com>
2017-09-11 07:25:09 +0800

    ...

--- db.js
+++ db.js
@@ -4,7 +4,10 @@ let Random = Mock.Random;
 module.exports = function() {
     var data = {
         news: [],
-        users: []
+        users: [],
+        SurplusData: [],
+        tblSysCcountData: [],
+        tblpage: []
     };
     [1, 2, 3].forEach(x => {
         data.users.push({
@@ -26,6 +29,49 @@ module.exports = function() {
             tag: Random.cword(2, 6),
             views: Random.integer(100, 5000),
             images: images.slice(0, Random.integer(1, 3))
+        })
+    }
+
+    for (var i = 0; i < 3; i++) {
+        data.SurplusData.push({
+            address: Random.city(),
+            centum: Random.integer(60, 100),
+            thousand: Random.integer(10, 50),
+            tenthousand: Random.integer(1, 10)
+        })
+    }
+
+    for (var i = 1; i <= 18; i++) {
+        data.tblpage.push({
+            rowindex: i,
+            applicationtime: Random.date(),
+            applicationpsn: Random.cname(),
+            tickettime: Random.date(),
+            ticketunit: Random.city(),
+            comeunit: Random.city(),
+            ticketcon: Random.ctitle(),
+            Subprime: Random.ctitle(3, 5),
+            gathering: Random.integer(10, 50),
+            area: Random.city(),
+            source: Random.city(),
+            way: Random.ctitle(1, 3)
+        })
+    }
+
+    for (var i = 1; i <= 18; i++) {
+        data.tblSysCcountData.push({
+            rowindex: i,
+            applicationtime: Random.date(),
+            applicationpsn: Random.cname(),
+            tickettime: Random.date(),
+            ticketunit: Random.city(),
+            comeunit: Random.city(),
+            ticketcon: Random.ctitle(),
+            Subprime: Random.ctitle(3, 5),
+            gathering: Random.integer(10, 50),
+            area: Random.city(),
+            source: Random.city(),
+            way: Random.ctitle(1, 3)
         })
     }
 
